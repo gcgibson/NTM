c8e5f66705633e4bf50723397b6771a14a529da4
C Gib <grahamcgibson@Caseys-MacBook-Air.local>
2014-12-07 15:51:58 -0600

    almost there

--- simpleCopycontroller.js
+++ simpleCopycontroller.js
@@ -2,25 +2,33 @@ var synaptic = require('synaptic');
 var memoryMatrix =require('./memoryMatrix');
 //initial memory matrix 
 var m_t = [[Math.random(),Math.random()],[Math.random(),Math.random()]];
-var w_t = [Math.random(),Math.random()];
+var w_t = [.5,.5];
 var r_t  =  readHeadConvolution(w_t,m_t);
+var sequenceTocopy = [1];	
 //train the neural network on the copy input task given a N=2 M=2 matrix
-var inputLayer = new synaptic.Layer(r_t.concat(1).length);
+var inputLayer = new synaptic.Layer(r_t.concat(0).length);
 var hiddenLayer = new synaptic.Layer(20);
 var headLayer = new synaptic.Layer(11);
-var outPutlayer = new synaptic.Layer(1);
+var outPutlayer = new synaptic.Layer(sequenceTocopy.length);
 var learningRate = .3;
-
- // connect the layers
     inputLayer.project(hiddenLayer);
     hiddenLayer.project(headLayer);
     headLayer.project(outPutlayer);
 
-for(var runTime = 0; runTime < 100; runTime++)
+var myNetwork = new synaptic.Network({
+    input: inputLayer,
+    hidden: [hiddenLayer,headLayer],
+    output: outPutlayer
+});
+
+ // connect the layers
+
+for(var runTime = 0; runTime < sequenceTocopy.length; runTime++)
 {	
     // when A activates 1
-   	inputLayer.activate(r_t.concat(0));   
- 	outPutlayer.propagate(learningRate, [0]);
+   	myNetwork.activate(r_t.concat(sequenceTocopy)); 
+
+ 	myNetwork.propagate(learningRate, sequenceTocopy);
  	
 
  	var emmitedHeadVector  = headLayer.activate();
@@ -31,19 +39,25 @@ for(var runTime = 0; runTime < 100; runTime++)
  	var tmp1 = memoryMatrix.generateContentBasedNormalization(emmitedHeadVector[0],emmitedHeadVector.slice(1,3),m_t);
   	var tmp2 = memoryMatrix.interpolationGate(emmitedHeadVector.slice(3,4),tmp1,w_t);
  	var tmp3 = memoryMatrix.convolutionalShift(emmitedHeadVector.slice(4,6),tmp2,m_t);
-	w_t = memoryMatrix.sharpening(tmp3,emmitedHeadVector.slice(6,7),m_t);
-	console.log('M_T old -----> ',m_t);
-	console.log('\n');
+	w_t = memoryMatrix.sharpening(tmp3,emmitedHeadVector.slice(6,7)*10,m_t);
+	
 	m_t= updateMemoryMatrix(m_t,w_t,emmitedHeadVector.slice(7,9),emmitedHeadVector.slice(9,11));
-	console.log('M_T New------->  ',m_t);
 	r_t  =  readHeadConvolution(w_t,m_t);
 	console.log('-------------------\n');
+	   	console.log('New r_t vector ------> ' ,r_t); 
+   	console.log('\n');
+   	 console.log('New w_t vector ------> ' , w_t); 
+   	console.log('\n');
+   		console.log('M_T New------->  ',m_t);
+
+ 
+
 	console.log('\n');
 }
 //in order to make the network differentiable you have to include the layers in 
 
 
-inputLayer.activate(r_t.concat(0)); 
+console.log('FINAL -----> ' ,myNetwork.activate(r_t.concat(sequenceTocopy	))); 
 
 
 
